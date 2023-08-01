e576c39f332188c641f482c9d68ab226f38091bc
AlGuron <blizgur8@gmail.com>
2023-08-01 19:35:36 +0300

    upd

--- src/telran/util/test/HashMapTest.java
+++ src/telran/util/test/HashMapTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class HashMapTest {
+import telran.util.HashMap;
 
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
+public class HashMapTest {
+
+//	@Override
+//	@BeforeEach
+//	 new HashMap<>();
+//		
 //	}
-
 }
