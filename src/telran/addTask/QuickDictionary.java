0e6e7dd123477c25b6d6d0150e26c70f48330d80
AlGuron <blizgur8@gmail.com>
2023-08-07 20:54:29 +0300

    ref

--- src/telran/addTask/QuickDictionary.java
+++ src/telran/addTask/QuickDictionary.java
@@ -12,6 +12,7 @@ public class QuickDictionary {
 	public QuickDictionary() {
 		for (int i = 0; i < lenghtTable.length; i++) {
 			lenghtTable[i] = new HashMap[CHAR_LENGHT];
+
 			for (int j = 0; j < CHAR_LENGHT; j++) {
 				lenghtTable[i][j] = new HashMap<String, String>();
 			}
@@ -24,11 +25,11 @@ public class QuickDictionary {
 		}
 
 		String res = null;
-		for (int i = 0; i < key.length(); i++) {
-			var test = lenghtTable[i][(int) (key.charAt(i) - 'A')];
-			res = test.put(key, value);
 
+		for (int i = 0; i < key.length(); i++) {
+			res = lenghtTable[i][key.charAt(i) - 'A'].put(key, value);
 		}
+
 		return res;
 	}
 
@@ -36,14 +37,17 @@ public class QuickDictionary {
 		if (!key.equals(key.toUpperCase())) {
 			throw new IllegalArgumentException();
 		}
+
 		HashMap<String, String> target = lenghtTable[0][key.charAt(0) - 'A'];
 		String res = null;
+
 		if (target != null) {
 			var targetKeys = target.keySet();
 
 			for (int i = 1; i < key.length(); i++) {
 				targetKeys.retainAll(lenghtTable[i][key.charAt(i) - 'A'].keySet());
 			}
+
 			var targetArr = targetKeys.toArray();
 			res = targetArr.length == 0 ? null : lenghtTable[0][key.charAt(0) - 'A'].get(targetArr[0]);
 		}
